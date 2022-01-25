574b7b94c8ba7026ea47e8e5604b16234c8a0fc1
Tyler Termini <tntermini@gmail.com>
2022-01-24 20:09:19 -0800

    change to imports in app.ts

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+const http_errors_1 = __importDefault(require("http-errors"));
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const morgan_1 = __importDefault(require("morgan"));
+const mongoose_1 = __importDefault(require("mongoose"));
 var indexRouter = require('./routes/index');
 var transactionRouter = require('./routes/transaction');
-var app = express();
-app.set('views', path.join(__dirname, 'views'));
+var app = (0, express_1.default)();
+app.set('views', path_1.default.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((0, morgan_1.default)('dev'));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/transaction', transactionRouter);
 app.use(function (req, res, next) {
-    next(createError(404));
+    next((0, http_errors_1.default)(404));
 });
 app.use(function (err, req, res, next) {
     res.locals.message = err.message;
@@ -27,11 +32,11 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 const uri = `mongodb+srv://primary:${process.env.MONGO_CLOUD_PASSWORD}@cluster0.5flqb.mongodb.net/main?retryWrites=true&w=majority`;
-mongoose.connect(uri);
-mongoose.connection.on('connected', () => {
+mongoose_1.default.connect(uri);
+mongoose_1.default.connection.on('connected', () => {
     console.log('connected to mongodb');
 });
-mongoose.connection.on('error', (err) => {
+mongoose_1.default.connection.on('error', (err) => {
     console.log('error connecting to mongodb ', err);
 });
 module.exports = app;
--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
 
 var indexRouter = require('./routes/index');
 var transactionRouter = require('./routes/transaction');

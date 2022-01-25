a169484e0f0151f044f1d2354681ade2e4ed81ba
Tyler Termini <tntermini@gmail.com>
2022-01-24 20:29:53 -0800

    Revert "change to imports in app.ts"
    This reverts commit 574b7b94c8ba7026ea47e8e5604b16234c8a0fc1.

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 require('dotenv').config()
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import mongoose from 'mongoose';
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var transactionRouter = require('./routes/transaction');

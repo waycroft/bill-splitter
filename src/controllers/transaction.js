3eb6a536711df44ca5a0b68f57899320d5b261a2
Tyler Termini <tntermini@gmail.com>
2022-01-30 12:00:30 -0800

    more add transactions logic

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -12,11 +12,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createTransaction = void 0;
 const Transaction_js_1 = require("../models/Transaction.js");
 const uuid_1 = require("uuid");
+const Pool_js_1 = require("../models/Pool.js");
+const exchange_id_js_1 = require("../helpers/exchange_id.js");
 function createTransaction(pool_id, transactionData) {
     return __awaiter(this, void 0, void 0, function* () {
         let freshId = (0, uuid_1.v4)();
+        transactionData.owner = yield (0, exchange_id_js_1.getObjId)("users", transactionData.owner);
+        transactionData.payees = yield Promise.all(transactionData.payees.map((uuid) => __awaiter(this, void 0, void 0, function* () {
+            return yield (0, exchange_id_js_1.getObjId)("users", uuid);
+        })));
         let transaction = new Transaction_js_1.TransactionModel(Object.assign({ id: freshId }, transactionData));
         yield transaction.save();
+        let pool = yield Pool_js_1.PoolModel.findOne({ id: pool_id });
+        if (pool && pool.transactions) {
+            if (pool.transactions.length >= 25) {
+                pool.transactions.shift();
+            }
+            pool.transactions.push(transaction);
+            yield pool.save();
+        }
         return transaction;
     });
 }
--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,16 +1,34 @@
-import { Document } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 import { Transaction, TransactionModel } from '../models/Transaction.js';
 import { v4 as uuidv4 } from 'uuid';
+import { Pool, PoolModel } from '../models/Pool.js';
+import { getObjId } from '../helpers/exchange_id.js';
 
 export async function createTransaction(pool_id: string, transactionData: Transaction) {
     let freshId = uuidv4();
-    let transaction: Document<Transaction> = new TransactionModel({
+    
+    // todo: turn "getObjId" into a function that traverses a whole object,
+    // scans for any uuids, and fetches the respective ObjIds. 
+    // to prevent having to call it multiple times on the same collection. 
+    transactionData.owner = await getObjId("users", transactionData.owner);
+    transactionData.payees = await Promise.all(transactionData.payees.map(async uuid => {
+        return await getObjId("users", uuid);
+    }))
+    
+    let transaction: HydratedDocument<Transaction> = new TransactionModel({
         id: freshId,
         ...transactionData
     });
     await transaction.save();
 
-    // todo: push to pool's transactions. if 25 transactions, then pop oldest and push newest
+    let pool: HydratedDocument<Pool> | null = await PoolModel.findOne({ id: pool_id });
+    if (pool && pool.transactions) {
+        if (pool.transactions.length >= 25) {
+            pool.transactions.shift();
+        }
+        pool.transactions.push(transaction);
+        await pool.save();
+    }
     
     return transaction;
 }

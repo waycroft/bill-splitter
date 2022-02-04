5dc5bd7d340dccb4f78475135adb0a4aa1573113
Tyler Termini <tntermini@gmail.com>
2022-02-04 15:34:13 -0800

    abandon object traversal id exchange recursive function for now...

--- src/helpers/exchange_id.js
+++ src/helpers/exchange_id.js
@@ -8,16 +8,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformUUIDsIntoObjectIds = exports.getUUID = exports.getObjId = void 0;
+exports.getUUID = exports.getObjId = void 0;
 const mongoose_1 = require("mongoose");
 const Pool_1 = require("../models/Pool");
 const Transaction_1 = require("../models/Transaction");
 const User_1 = require("../models/User");
-const uuid_validate_1 = __importDefault(require("uuid-validate"));
 const allModels = {
     "pools": Pool_1.PoolModel,
     "transactions": Transaction_1.TransactionModel,
@@ -51,55 +47,4 @@ const getUUID = function (collectionName, id) {
     });
 };
 exports.getUUID = getUUID;
-const transformUUIDsIntoObjectIds = (collectionName, object) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log('start object', object);
-    if (typeof object === "string" && (0, uuid_validate_1.default)(object)) {
-        const model = allModels[collectionName];
-        const doc = yield model.findOne({ id: object }).lean();
-        return doc._id;
-    }
-    else if (typeof object === "string") {
-        console.log('not valid UUID');
-    }
-    if (typeof object !== "object" && !Array.isArray(object)) {
-        console.log('not object or array');
-        return 0;
-    }
-    else if (typeof object === "object" && !Array.isArray(object)) {
-        if (Object.keys(object).length === 0) {
-            console.log('Is an object, but has no properties');
-            return 0;
-        }
-        console.log('found a nested object...');
-        for (let i = 0; i < Object.keys(object).length; i++) {
-            let keys = Object.keys(object);
-            let currentKey = keys[i];
-            let nextKey = keys[i + 1];
-            let currentVal = object[currentKey];
-            console.log('property', currentVal);
-            let parseCurrentVal = yield (0, exports.transformUUIDsIntoObjectIds)(collectionName, currentVal);
-            if ((0, mongoose_1.isValidObjectId)(parseCurrentVal)) {
-                object[currentKey] = parseCurrentVal;
-            }
-            else {
-                yield (0, exports.transformUUIDsIntoObjectIds)(collectionName, object[nextKey]);
-            }
-        }
-    }
-});
-exports.transformUUIDsIntoObjectIds = transformUUIDsIntoObjectIds;
-var test = {
-    one: 1,
-    two: "two",
-    three: {
-        type: "string",
-        value: 3
-    },
-    four: [1, 2, 3],
-    five: "f28be967-fc74-48c6-a112-bb9d87ab0dc7"
-};
-(0, exports.transformUUIDsIntoObjectIds)("pools", test)
-    .then(() => {
-    console.log(test);
-});
 //# sourceMappingURL=exchange_id.js.map
--- src/helpers/exchange_id.ts
+++ src/helpers/exchange_id.ts
@@ -2,7 +2,6 @@ import { Document, isValidObjectId, Schema } from "mongoose";
 import { PoolModel } from "../models/Pool";
 import { TransactionModel } from "../models/Transaction";
 import { UserModel } from "../models/User";
-import validate from 'uuid-validate';
 
 const allModels = {
     "pools": PoolModel,
@@ -39,59 +38,3 @@ export const getUUID = async function(collectionName: string, id: Schema.Types.O
 
     return doc.id;
 }
-
-export const transformUUIDsIntoObjectIds = async (collectionName: string, object: any) => {
-    console.log('start object', object);
-    if (typeof object === "string" && validate(object)) {
-        const model = allModels[collectionName];
-        const doc = await model.findOne({ id: object }).lean();
-        return doc._id;
-    } else if (typeof object === "string") {
-        console.log('not valid UUID');
-    }
-    // todo: terminate when all keys have been searched. how to identify...?
-    // not an object or an array
-    if (typeof object !== "object" && !Array.isArray(object)) {
-        console.log('not object or array');
-        return 0
-    // object, but not array
-    } else if (typeof object === "object" && !Array.isArray(object)) {
-        // object has no properties... (aka: {})
-        if (Object.keys(object).length === 0) {
-            console.log('Is an object, but has no properties');
-            return 0
-        }
-        // object has properties. recur from here
-        console.log('found a nested object...');
-        for (let i = 0; i < Object.keys(object).length; i++) {
-            let keys = Object.keys(object);
-            let currentKey = keys[i]
-            let nextKey = keys[i+1]
-            let currentVal = object[currentKey];
-            console.log('property', currentVal);
-
-            let parseCurrentVal = await transformUUIDsIntoObjectIds(collectionName, currentVal);
-            if (isValidObjectId(parseCurrentVal)) {
-                object[currentKey] = parseCurrentVal; 
-            } else {
-                await transformUUIDsIntoObjectIds(collectionName, object[nextKey]);
-            }
-        }
-    }
-}
-
-var test = {
-    one: 1,
-    two: "two",
-    three: {
-        type: "string",
-        value: 3
-    },
-    four: [1,2,3],
-    five: "f28be967-fc74-48c6-a112-bb9d87ab0dc7"
-}
-
-transformUUIDsIntoObjectIds("pools", test)
-.then(() => {
-    console.log(test);
-});

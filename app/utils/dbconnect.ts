b0b814474b61d3cb6412d68157a7fadbfe1bfa7d
Tyler Termini <tntermini@gmail.com>
2022-02-19 13:16:10 -0800

    simplify dbconnect

--- app/utils/dbconnect.ts
+++ app/utils/dbconnect.ts
@@ -3,11 +3,5 @@ import invariant from "tiny-invariant";
 
 export default async function() {
     invariant(process.env.MONGO_CONNECTION_STRING, "No MONGO_CONNECTION_STRING declared in env, could not connect");
-    try {
-        await connect(process.env.MONGO_CONNECTION_STRING);
-        console.log('connected to mongodb');
-    } catch (error) {
-       console.log('error connecting to mongodb:', error) 
-    }
-    
+    await connect(process.env.MONGO_CONNECTION_STRING);
 }

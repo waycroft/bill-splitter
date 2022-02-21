d2186bfaac4a79275fe84cea3e0e7b2e0fbf6c31
Tyler Termini <tntermini@gmail.com>
2022-02-20 21:29:40 -0800

    search resource route works when it's a POST

--- app/routes/users/search.tsx
+++ app/routes/users/search.tsx
@@ -1,15 +1,14 @@
-import type { LoaderFunction } from 'remix';
+import type { ActionFunction } from 'remix';
 import { searchAllUsers } from '~/utils/user.actions';
 
-export let loader: LoaderFunction =  async ({ request }) => {
-    let url = new URL(request.url);
-    let query = url.searchParams.get("q");
+export let action: ActionFunction =  async ({ request }) => {
+    let formData = await request.formData();
+    let query = formData.get('q');
 
     if (!query) {
         return null;
     }
 
-    let results = await searchAllUsers(query, { lean: true });
-    return { results: results };
+    let results = await searchAllUsers(query.toString(), { lean: true });
+    return results;
 }
-

87a596081d71119e713a0bec41ba3d1705af02a3
Tyler Termini <tntermini@gmail.com>
2022-02-23 17:46:41 -0800

    new transaction route

--- app/routes/pools/$poolId/transactions/new.tsx
+++ app/routes/pools/$poolId/transactions/new.tsx
@@ -1,4 +1,10 @@
-import { Form } from 'remix';
+import { ActionFunction, Form } from "remix";
+
+const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const { total, isSplitEvenly } = Object.fromEntries(formData);
+  // todo: form validation
+};
 
 export default function NewTransactionRoute() {
   return (
@@ -7,10 +13,32 @@ export default function NewTransactionRoute() {
         <h1 className="text-4xl font-bold">Add Transaction</h1>
       </div>
       <div>
-        <Form method='post'>
-          {/* <input type='number' name='' */}
+        <Form method="post">
+          <fieldset className="input-group">
+            <label htmlFor="total" className="label">
+              Total amount:
+            </label>
+            <input
+              type="number"
+              name="total"
+              defaultValue={0}
+              className="input border-base-200"
+            />
+            <label htmlFor="isSplitEvenly" className="label">
+              Splitting evenly?
+            </label>
+            <input
+              type="checkbox"
+              name="isSplitEvenly"
+              value="checked"
+              className="checkbox"
+            />
+            <button className="btn btn-primary rounded-md">
+              Create Transaction
+            </button>
+          </fieldset>
         </Form>
       </div>
     </div>
-  )
+  );
 }

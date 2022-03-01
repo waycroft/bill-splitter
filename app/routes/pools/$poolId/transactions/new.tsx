4cfb12e272f3269b057f8ef110cf94d730c7a550
Tyler Termini <tntermini@gmail.com>
2022-02-28 16:36:22 -0800

    refactoring step logic, added hidden fields

--- app/routes/pools/$poolId/transactions/new.tsx
+++ app/routes/pools/$poolId/transactions/new.tsx
@@ -1,15 +1,17 @@
-// todo: extract each step (label + input combo) into its own component,
+// todo: architecture: extract each step (label + input combo) into its own component,
 // and use a map/enum to switch them on/off. Maybe a prop for each that accepts
 // a map defined at the root of this route component that decides its hidden/shown status
 import { Form, redirect, useLoaderData, useSearchParams } from "remix";
 import invariant from "tiny-invariant";
 import { getPool } from "~/utils/pool.actions";
-
-import type { LoaderFunction, ActionFunction } from "remix";
-import { upsertTransaction } from "~/utils/transactions.actions";
+import { insertTransaction } from "~/utils/transactions.actions";
 import { PayeeData } from "~/models/TransactionSchema";
 import CustomSplitItemList from "~/components/CustomSplitItemList";
 import { Pool } from "~/models/PoolSchema";
+import { LeanUser } from "~/models/UserSchema";
+import mongoose, { isValidObjectId } from "mongoose";
+
+import type { LoaderFunction, ActionFunction } from "remix";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.poolId, "Could not read $poolId in path params");
@@ -19,33 +21,49 @@ export const loader: LoaderFunction = async ({ params }) => {
 };
 
 interface RawTransactionInput {
-  total: number;
-  isSplitEvenly: boolean;
-  memberCount: number;
-  transaction_date: string;
+  totalAmountInput: number;
+  categoryInput: string;
+  memoInput: string;
   owner: string;
-  owner_amount: string;
-  category: string;
-  memo: string;
-  payees: PayeeData[];
+  payees: string;
 }
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const { poolData, total, owner } = Object.fromEntries(formData);
-  // todo: form validation
+  const { totalAmountInput, categoryInput, memoInput, owner, payees } =
+    Object.fromEntries(formData);
+  const newTransaction = {
+    // todo: feature: actually allow user input for transaction date
+    transaction_date: new Date(),
+    created_at: new Date(),
+    total: Number(totalAmountInput),
+    owner: new mongoose.Types.ObjectId(owner.toString()),
+    category: categoryInput,
+    memo: memoInput,
+    payees: payees
+      .toString()
+      .split(",")
+      .map((objIdInput: FormDataEntryValue) => {
+        let objIdStr = objIdInput.toString();
+        if (isValidObjectId(objIdStr)) {
+          return new mongoose.Types.ObjectId(objIdStr);
+        }
+      })
+      .filter((ele) => {
+        return ele != undefined;
+      }),
+  };
+  // todo: validation: form validation
   return redirect("new");
 };
 
 export default function NewTransactionRoute() {
   const poolData: Pool = useLoaderData();
-  console.log(poolData);
   const [searchParams] = useSearchParams();
   const step = Number(searchParams.get("step"));
-  const isSplitEvenPath = !!searchParams.get("path::splitEven");
-  console.log("isSplitEvenPath:", isSplitEvenPath);
+  const isSplitEvenPath = searchParams.get("path") === "splitEvenly";
   const isFinalStep =
-    (isSplitEvenPath && step === 3) || (!isSplitEvenPath && step === 7);
+    (isSplitEvenPath && step === 3) || (!isSplitEvenPath && step === 2.2);
   invariant(step != 0, "step search param cannot be null");
   return (
     <div>
@@ -55,29 +73,51 @@ export default function NewTransactionRoute() {
       <div>
         <Form method={isFinalStep ? "post" : "get"}>
           <fieldset className="input-group">
+            {/* meta fields */}
             <input
               hidden
               readOnly
               name="step"
               value={isSplitEvenPath ? step + 1 : step + 0.1}
             />
+            <input
+              hidden
+              readOnly
+              name="owner"
+              // todo: authentication: this will be the currently authenticated user
+              value={"6200824a07f36f60231a5377"}
+            />
+            <input
+              hidden
+              readOnly
+              name="payees"
+              // todo: authentication: this will be the opposite of the currently authenticated user
+              value={poolData.members
+                .map((user: LeanUser) => {
+                  if (user._id.toString() !== "6200824a07f36f60231a5377") {
+                    return user._id.toString();
+                  }
+                })
+                .toString()}
+            />
+            {/* real input fields */}
             <label
               htmlFor="isSplitEvenly"
-              className={`label ${step !== 1 ? "hidden" : ""}`}
+              className={`label ${step === 1 ? "" : "hidden"}`}
             >
               Splitting evenly?
             </label>
             <input
               defaultChecked
-              name="path::splitEven"
+              name="path"
               type="checkbox"
-              hidden={step !== 1 ? true : false}
+              hidden={step === 1 ? false : true}
               className="checkbox"
-              value="true"
+              value="splitEvenly"
             />
             <label
               htmlFor="totalAmountInput"
-              className={`label ${step !== 2 ? "hidden" : ""}`}
+              className={`label ${step === 2 ? "" : "hidden"}`}
             >
               Total amount?
             </label>
@@ -85,17 +125,17 @@ export default function NewTransactionRoute() {
               name="totalAmountInput"
               type="number"
               id="totalAmountInput"
-              defaultValue={0}
+              placeholder="420"
               className="input"
-              hidden={step !== 2 ? true : false}
+              hidden={step === 2 ? false : true}
             />
             <label
               htmlFor="customSplitInput"
-              className={`label ${step !== 2.1 ? "hidden" : ""}`}
+              className={`label ${step === 2.1 ? "" : "hidden"}`}
             >
               Custom split
             </label>
-            <div className={`${step !== 2.1 ? "hidden" : ""}`}>
+            <div className={`${step === 2.1 ? "" : "hidden"}`}>
               <CustomSplitItemList
                 id="customSplitInput"
                 name="customSplitInput"
@@ -103,17 +143,18 @@ export default function NewTransactionRoute() {
             </div>
             <label
               htmlFor="categoryInput"
-              className={`label ${step !== 3 ? "hidden" : ""}`}
+              className={`label ${isFinalStep ? "" : "hidden"}`}
             >
               Category / Memo
             </label>
+            {/* todo: feature: this will be a single-select. data loaded from categories stored in poolData */}
             <input
               name="categoryInput"
               type="text"
               id="categoryInput"
               placeholder="Enter category here"
               className="input"
-              hidden={step !== 3 ? true : false}
+              hidden={isFinalStep ? false : true}
             />
             <input
               name="memoInput"
@@ -121,9 +162,14 @@ export default function NewTransactionRoute() {
               id="memoInput"
               placeholder="Enter memo here"
               className="input"
-              hidden={step !== 3 ? true : false}
+              hidden={isFinalStep ? false : true}
             />
-            <button type="submit" className="btn btn-secondary rounded-md">
+            <button
+              type="submit"
+              className={`btn btn-secondary rounded-md ${
+                isFinalStep ? "hidden" : ""
+              }`}
+            >
               Next 👉
             </button>
             <button

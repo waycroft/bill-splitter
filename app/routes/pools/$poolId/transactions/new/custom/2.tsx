f9748aa93b9b7baf2eb21100a30082e1bbc2c627
Tyler Termini <tntermini@gmail.com>
2022-03-05 14:00:55 -0800

    pt 2

--- app/routes/pools/$poolId/transactions/new/custom/2.tsx
+++ app/routes/pools/$poolId/transactions/new/custom/2.tsx
@@ -1,17 +1,20 @@
-// todo: architecture: extract each step (label + input combo) into its own component,
-// and use a map/enum to switch them on/off. Maybe a prop for each that accepts
-// a map defined at the root of this route component that decides its hidden/shown status
-import invariant from "tiny-invariant";
 import { Form, redirect, useLoaderData, useTransition } from "remix";
+import invariant from "tiny-invariant";
+import { v4 as uuidv4 } from "uuid";
+import { useReducer } from "react";
 import { getPool } from "~/utils/pool.actions";
 import { Pool } from "~/models/PoolSchema";
 import { User } from "~/models/UserSchema";
 import { getUser } from "~/utils/user.actions";
+import LoaderDataHiddenInput from "~/components/util/LoaderDataHiddenInput";
+import CustomSplitItem from "~/components/CustomSplitItem";
+import { debounce } from "lodash";
+import XButton from "~/components/XButton";
 
 import type { LoaderFunction, ActionFunction } from "remix";
 import type { LoaderDataShape } from "../index";
-import LoaderDataHiddenInput from "~/components/util/LoaderDataHiddenInput";
-import CustomSplitItemList from "~/components/CustomSplitItemList";
+import type { CustomSplitItemData } from "~/components/CustomSplitItem";
+import type { ReducerAction } from "~/components/CustomSplitItem";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.poolId, "Could not read $poolId in path params");
@@ -25,22 +28,114 @@ export const loader: LoaderFunction = async ({ params }) => {
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const { poolData, currentUserData } = Object.fromEntries(formData);
+  console.log(formData);
   const currentUser: User = JSON.parse(currentUserData.toString());
   const pool: Pool = JSON.parse(poolData.toString());
   invariant(currentUser, "currentUser is undefined/null");
+  invariant(pool, "pool is undefined/null");
   return redirect(`/pools/${pool._id}`);
+};
+
+const reducer = (state: CustomSplitItemData[], action: ReducerAction) => {
+  switch (action.type) {
+    case "ADD":
+      return state.concat(action.payload);
+    case "REMOVE":
+      return state.filter((item) => {
+        return item.id !== action.itemToRemoveId;
+      });
+    case "UPDATE":
+      return state;
+    default:
+      throw new Error("Missing/bad reducer action");
+  }
 };
 
 export default function CustomSplitStep2() {
   const loaderData = useLoaderData<LoaderDataShape>();
   const transition = useTransition();
+  const initial: CustomSplitItemData[] = [
+    {
+      id: uuidv4(),
+      itemName: "garlic fries",
+      amount: 0,
+      payees: loaderData.poolData.members,
+    },
+  ];
+  function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log("changed: ", e);
+    // possible to just input.get the values of all the inputs instead 
+    // of messing around with switch-ing the target of the input?
+    // dispatch({
+    //   type: "UPDATE",
+    //   payload: e.target.value,
+    // });
+  }
+  const [state, dispatch] = useReducer(reducer, initial);
   return (
     <div>
+      <h1>Custom Split</h1>
+      {/* these inputs are not submitted with the form, but rather are passed to the hidden inputs below in the <Form> which actually submit the state */}
+      <div>
+        {state.map((item) => {
+          return (
+            <div key={item.id}>
+              <CustomSplitItem
+                id={item.id}
+                itemName={item.itemName}
+                amount={item.amount}
+                payees={item.payees}
+                changeHandler={debounce(changeHandler, 1000)}
+              />
+              <button
+                className="btn btn-primary rounded-full gap-1"
+                type="button"
+                onClick={() =>
+                  dispatch({ type: "REMOVE", itemToRemoveId: item.id })
+                }
+              >
+                <XButton />
+              </button>
+            </div>
+          );
+        })}
+        <button
+          name="addItem"
+          type="button"
+          onClick={() =>
+            dispatch({
+              type: "ADD",
+              payload: {
+                id: uuidv4(),
+                // todo: fun: random name each new item
+                itemName: "mocha matcha",
+                amount: 0,
+                payees: loaderData.poolData.members,
+              },
+            })
+          }
+          className="btn btn-accent rounded"
+        >
+          Add another
+        </button>
+        {/* todo: validation: display remaining amount as user adds splits, like YNAB. This will also be part of the form validation */}
+      </div>
       <Form method="post">
         {/* todo: refactor: is it possible to pass a type param to loaderData here? */}
         <LoaderDataHiddenInput loaderData={loaderData} />
-        <label htmlFor="categoryInput">Category / Memo</label>
-        <CustomSplitItemList poolData={loaderData.poolData} />
+        {state.map((splitItem: CustomSplitItemData) => {
+          return (
+            <fieldset key={splitItem.id}>
+              <input
+                hidden
+                readOnly
+                type="text"
+                name="splitItems"
+                value={JSON.stringify(splitItem)}
+              />
+            </fieldset>
+          );
+        })}
         <button type="submit" className={`btn btn-secondary rounded-md`}>
           {transition.state === "submitting" ? "Nexting..." : "Next 👉"}
         </button>
